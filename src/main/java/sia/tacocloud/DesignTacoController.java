7a4d63d9e18239e8cef13cd1c3327ba0e6952dad
leonredman <leonproart@yahoo.com>
2023-10-18 22:36:25 -0700

    add controller methods

--- src/main/java/sia/tacocloud/DesignTacoController.java
+++ src/main/java/sia/tacocloud/DesignTacoController.java
@@ -3,10 +3,7 @@ package sia.tacocloud;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import sia.tacocloud.Ingredient.Type;
 
 import java.util.Arrays;
@@ -49,5 +46,12 @@ public class DesignTacoController {
     private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
     return ingredients .stream() .filter(x -> x.getType().equals(type)) .collect(Collectors.toList());
 
+ }
+ @PostMapping
+ public String processTaco(Taco taco,
+    @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect: orders/current";
  }
 }
